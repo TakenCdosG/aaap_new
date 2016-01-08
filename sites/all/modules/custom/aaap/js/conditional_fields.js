bfd4812ae2d7dd75fa713e795bf9bb390f14b6f0
Adrián Morelos <ing.adrianmorelos@gmail.com>
2016-01-08 18:42:46 -0500

    Ajustes

--- sites/all/modules/custom/aaap/js/conditional_fields.js
+++ sites/all/modules/custom/aaap/js/conditional_fields.js
@@ -8,12 +8,13 @@
 
         var $form = $("#user-register-form");
         var $select = $('#edit-field-us-province-state-und', $form);
+        var $region = $("#edit-field-us-region-und", $form);
         var south = ["AL", "AR", "DE", "FL", "GA" , "KY" , "LA" , "MD" , "MS" , "NC" , "OK" , "SC" , "TN" , "TX" , "VA" , "WA" , "WV"];
         var northeast = ["CT", "ME", "MA", "NH", "NJ", "NY", "PA", "RI", "VT"];
         var midwest = ["IL", "IN", "IA", "KS", "MI", "MN", "MO", "NE", "ND", "OH", "SD", "WI"];
         var west = ["AK", "AZ", "CA", "CO", "HI", "ID", "MT", "NM", "NV", "OR", "UT", "WY"];
 
-        $select.attr('disabled', 'disabled');
+        $region.attr('disabled', 'disabled');
 
         $select.change(function() {
             var selected = $(this).val();
