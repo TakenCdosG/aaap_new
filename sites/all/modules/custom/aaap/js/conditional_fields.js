9b1cd7a25614b8f2c162f97ccce59506c8ab2873
Adrián Morelos <ing.adrianmorelos@gmail.com>
2016-01-08 18:16:50 -0500

    Ajustes

--- sites/all/modules/custom/aaap/js/conditional_fields.js
+++ sites/all/modules/custom/aaap/js/conditional_fields.js
@@ -1,1 +1,16 @@
-console.log('-> attached');
+/*
+ * @file
+ * JavaScript for Conditional Field in From.
+ *
+ */
+(function ($) {
+    $(document).ready(function () {
+
+        var $form = $("#user-register-form");
+        $('#edit-field-us-province-state-und', $form).change(function() {
+            var selected = $(this).val();
+            console.log('-> Selected value: ' + selected);
+        });
+
+    });
+})(jQuery);
