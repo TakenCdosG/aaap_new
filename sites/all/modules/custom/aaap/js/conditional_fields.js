e1e14ebec43ac1d16001b7c4d78dfa57a1c76c4a
Adrián Morelos <ing.adrianmorelos@gmail.com>
2016-01-08 18:40:42 -0500

    Ajustes

--- sites/all/modules/custom/aaap/js/conditional_fields.js
+++ sites/all/modules/custom/aaap/js/conditional_fields.js
@@ -7,9 +7,37 @@
     $(document).ready(function () {
 
         var $form = $("#user-register-form");
-        $('#edit-field-us-province-state-und', $form).change(function() {
+        var $select = $('#edit-field-us-province-state-und', $form);
+        var south = ["AL", "AR", "DE", "FL", "GA" , "KY" , "LA" , "MD" , "MS" , "NC" , "OK" , "SC" , "TN" , "TX" , "VA" , "WA" , "WV"];
+        var northeast = ["CT", "ME", "MA", "NH", "NJ", "NY", "PA", "RI", "VT"];
+        var midwest = ["IL", "IN", "IA", "KS", "MI", "MN", "MO", "NE", "ND", "OH", "SD", "WI"];
+        var west = ["AK", "AZ", "CA", "CO", "HI", "ID", "MT", "NM", "NV", "OR", "UT", "WY"];
+
+        $select.attr('disabled', 'disabled');
+
+        $select.change(function() {
             var selected = $(this).val();
-            console.log('-> Selected value: ' + selected);
+            var inArray = -1;
+            // SOUTH
+            inArray = south.indexOf(selected);
+            if(inArray != -1){
+                $("#edit-field-us-region-und", $form).val("SOUTH");
+            }
+            // NORTHEAST
+            inArray = northeast.indexOf(selected);
+            if(inArray != -1){
+                $("#edit-field-us-region-und", $form).val("NORTHEAST");
+            }
+            // MIDWEST
+            inArray = midwest.indexOf(selected);
+            if(inArray != -1){
+                $("#edit-field-us-region-und", $form).val("MIDWEST");
+            }
+            // WEST
+            inArray = west.indexOf(selected);
+            if(inArray != -1){
+                $("#edit-field-us-region-und", $form).val("WEST");
+            }
         });
 
     });
