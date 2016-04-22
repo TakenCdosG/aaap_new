1f293cba9e208e061f4a231730f186bc148e177b
Adrián Andres Morelos Henriquez <ing.adrianmorelos@gmail.com>
2016-04-22 12:24:06 -0500

    AAAP wants to update the text on the email that gets sent once a member application is approved

--- sites/all/modules/config/membership_aplication_aproved.php
+++ sites/all/modules/config/membership_aplication_aproved.php
@@ -1,9 +1,8 @@
 <?php
-mail($edit['mail'], SITE_NAME.' Membership Application Approved; Pending Payment','Your application for '.SITE_NAME.' membership has been approved. Please remit your membership dues by check or credit card.'."\r\n \r\n"
-.'Mail Checks to:'. "\r\n"
-    .'Eileen Drames'."\r\n"
-    .'The Children’s Hospital of Philadelphia'. "\r\n"
-    .'General Pediatrics, 12NW Room 57'. "\r\n"
+mail($edit['mail'], SITE_NAME.' Membership Application Approved; Pending Payment','Thank you for submitting your '.SITE_NAME.' Membership application which has now been reviewed and will be finalized once we receive your payment. Please log on to your account and pay using a credit card by clicking on the link below. If you will be paying by check, please allow approximately 4 weeks for processing and remit  payment to the '.SITE_NAME.' Treasurer at the address below. '."\r\n \r\n"
+.SITE_NAME.' Treasurer: Eileen Drames'. "\r\n"
+    .'c/o The Children’s Hospital of Philadelphia'. "\r\n"
+    .'General Pediatrics, 12NW Room 57'."\r\n"
     .'34th and Civic Center Boulevard'. "\r\n"
     .'Philadelphia, PA 19104.'."\r\n \r\n"
 .'Registration Receipt'. "\r\n"
@@ -17,11 +16,9 @@ mail($edit['mail'], SITE_NAME.' Membership Application Approved; Pending Payment
 .'City: '.$user_data->field_city['und'][0]['value']. "\r\n"
 .'State/Province: '.$user_data->field_state['und'][0]['value']. "\r\n"
 .'Zip/Postal Code: '.$user_data->field_zip['und'][0]['value']. "\r\n \r\n"
-
-
 .'Make a Credit Card Payment:'. "\r\n"
 .$onetime. "\r\n \r\n"
-
+.'If you have any questions please contact our AAAP Membership Director Michelle De Guire: mdeguire@ucsd.edu'. "\r\n"
 .'Thank you,'. "\r\n"
 .SITE_NAME, HEADERS);
 
