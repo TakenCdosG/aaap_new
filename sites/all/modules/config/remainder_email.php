f7fa2958067333656fc0ff543fea104b555e70e6
Giovanny Sayas <giovanny.sayas@akendos.com>
2017-01-03 10:08:08 -0500

    reminder email updated - membership lapsed

--- sites/all/modules/config/remainder_email.php
+++ sites/all/modules/config/remainder_email.php
@@ -1,12 +1,12 @@
 <?php
-mail($user->mail,'AAAP Renewal - Time to Renew Your Membership' ,'AAAP Members,'."\r\n\n"
-    .'It is time to renew your annual AAAP Membership for 2017!'. "\r\n\n"
+mail($user->mail,'AAAP Renewal - Membership has Lapsed, Renew Now' ,'AAAP Members,'."\r\n\n"
+    .'Your AAAP Membership has now LAPSED. Please follow the steps below to renew your annual AAAP Membership for 2017.'. "\r\n\n"
     .'Amount Due: $600 '. "\r\n\n"
 	.'Steps for Renewal:'. "\r\n"
     .'1. Go to the AAAP website www.aaapeds.org.'. "\r\n"
 	.'2. Sign-in using your AAAP email and password.'. "\r\n"
     .'3. Select Membership Renewal in the Membership tab'. "\r\n\n"
-    .'Trouble accessing your account?  If you haven\'t accessed your account recently or have forgotten your password, use the "forgot password" feature or contact mdeguire@ucsd.edu.'. "\r\n\n"
+    .'Trouble accessing your account? If you haven\'t accessed your account recently or have forgotten your password, use the "forgot password" feature or contact mdeguire@ucsd.edu.'. "\r\n\n"
     .'Thank You, '. "\n"
     .'Michelle DeGuire'. "\n"
     .'AAAP Membership Director'
